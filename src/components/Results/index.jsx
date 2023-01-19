64ce5becfb364f3639dcb4778bac72a532b2125e
Ciro Cativo <cirocativo@gmail.com>
2023-01-19 14:27:02 -0400

    feat: results only appears when response is received

--- src/components/Results/index.jsx
+++ src/components/Results/index.jsx
@@ -3,20 +3,22 @@ import "./styles.css";
 
 export default function Results({ response }) {
   return (
-    <div className="resultsContainer">
-      <h2>VOCÊ RECEBERÁ:</h2>
-      <p>
-        Amanhã: <strong>R$ {response[1]},00</strong>
-      </p>
-      <p>
-        Em 15 dias: <strong>R$ {response[15]},00</strong>
-      </p>
-      <p>
-        Em 30 dias: <strong>R$ {response[30]},00</strong>
-      </p>
-      <p>
-        Em 90 dias: <strong>R$ {response[90]},00</strong>
-      </p>
-    </div>
+    response[1] && (
+      <div className="resultsContainer">
+        <h2>VOCÊ RECEBERÁ:</h2>
+        <p>
+          Amanhã: <strong>R$ {response[1]},00</strong>
+        </p>
+        <p>
+          Em 15 dias: <strong>R$ {response[15]},00</strong>
+        </p>
+        <p>
+          Em 30 dias: <strong>R$ {response[30]},00</strong>
+        </p>
+        <p>
+          Em 90 dias: <strong>R$ {response[90]},00</strong>
+        </p>
+      </div>
+    )
   );
 }
